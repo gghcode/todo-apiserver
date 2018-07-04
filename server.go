ea07337e7e423925f62b142491fc2a8158d416c0
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-07-04 19:10:15 +0900

    Apply configuration logic

--- server.go
+++ server.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"apas-todo-service/config"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+)
+
 func main() {
-	
+	configuration := config.Load()
+
+	session, err := mgo.Dial(configuration.MongoConnStr)
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+
+	router := gin.Default()
+	router.Run(fmt.Sprintf(":%d", configuration.Port))
 }
