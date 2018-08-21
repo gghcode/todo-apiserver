aa609bcda996d52a2c1c5f1b4b8a568c9e79d9d3
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-08-21 22:52:51 +0900

    Simplify main process

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"apas-todo-apiserver/app"
+	logger "github.com/Sirupsen/logrus"
+	"github.com/pkg/errors"
+)
 
+func main() {
+	builder := app.NewViperConfigurationBuilder()
+	builder.SetBasePath(".")
+	builder.AddJsonFile("config")
+	builder.AddEnvironmentVariables()
+
+	configuration, err := builder.Build()
+	if err != nil {
+		logger.Fatalln(errors.Wrap(err, "Configuration build failed."))
+	}
+
+	logger.Infoln(configuration)
 }
