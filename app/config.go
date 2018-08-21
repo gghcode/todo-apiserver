cf94ebe6668ff30abbe92ce83ab572fb51b28819
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-08-21 12:33:55 +0900

    Change abstract configuration system

--- app/config.go
+++ app/config.go
@@ -1,25 +1,10 @@
 package app
 
-import "github.com/spf13/viper"
+type Configuration = map[string]interface{}
 
-type Config struct {
-	Port int
-	MongoConnStr string
-}
-
-func loadConfig() (*Config, error) {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-
-	if err := viper.ReadInConfig(); err != nil {
-		return nil, err
-	}
-
-	var result *Config
-
-	if err := viper.Unmarshal(&result); err != nil {
-		return nil, err
-	}
-
-	return result, nil
+type ConfigurationBuilder interface {
+	SetBasePath(path string)
+	AddJsonFile(path string)
+	AddEnvironmentVariables()
+	Build() (Configuration, error)
 }
