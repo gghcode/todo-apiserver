ab3c8ee71740ba2c49a3c4af61e3e0561426e719
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-09-15 15:42:38 +0900

    Decouple gin engine and app server

--- app/server.go
+++ app/server.go
@@ -4,13 +4,12 @@ import (
 	"apas-todo-apiserver/controllers"
 	"fmt"
 	"github.com/Sirupsen/logrus"
-	"github.com/gin-gonic/gin"
 )
 
 type TodoApiServer struct {
-	engine        *gin.Engine
-	logger        *logrus.Entry
+	engine        ServerEngine
 	configuration Configuration
+	logger        *logrus.Entry
 }
 
 func NewServer(configuration Configuration) *TodoApiServer {
@@ -23,9 +22,7 @@ func NewServer(configuration Configuration) *TodoApiServer {
 }
 
 func (apiServer *TodoApiServer) Initialize() {
-	engine := gin.New()
-	engine.Use(gin.Logger())
-	engine.Use(gin.Recovery())
+	engine := NewGinEngine()
 
 	setRoutes(engine, getControllerList())
 
@@ -38,17 +35,17 @@ func getControllerList() []controllers.Controller {
 	}
 }
 
-func setRoutes(router *gin.Engine, controllerList []controllers.Controller) {
+func setRoutes(router ServerEngine, controllerList []controllers.Controller) {
 	for _, controller := range controllerList {
 		addRoutes(router, controller)
 	}
 }
 
-func addRoutes(router *gin.Engine, controller controllers.Controller) {
-	handlers := controller.GetHandlers()
+func addRoutes(router ServerEngine, controller controllers.Controller) {
+	handlers := controller.GetHandlerInfos()
 
 	for _, handler := range handlers {
-		router.Handle(handler.Method, handler.Path, handler.Handle)
+		router.Handle(handler)
 	}
 }
 
