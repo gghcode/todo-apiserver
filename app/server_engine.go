2de6e4a190c0dab11eac516fb83c77f6947c8246
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2018-09-15 15:38:46 +0900

    Add method that handle http request

--- app/server_engine.go
+++ app/server_engine.go
@@ -1,13 +1,21 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"apas-todo-apiserver/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 type ServerEngine interface {
+	Handle(handlerInfo controllers.HandlerInfo)
 	Run(addr string) error
 }
 
 type GinEngine struct {
 	gin *gin.Engine
+}
+
+func (engine *GinEngine) Handle(info controllers.HandlerInfo) {
+	engine.gin.Handle(info.Method, info.Path, info.Handle)
 }
 
 func (engine *GinEngine) Run(addr string) error {
