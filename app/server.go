2931f4d3c7323146535edc234d168143e959687e
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-04-15 08:58:38 +0900

    Include conf to server

--- app/server.go
+++ app/server.go
@@ -8,16 +8,21 @@ import (
 // Server is api-server instance. it contains gin.Engine, middlewares, configuration.
 type Server struct {
 	core          *gin.Engine
-	configuration *config.Configuration
+	configuration config.Configuration
+}
+
+// New return new server instance.
+func New(conf config.Configuration) *Server {
+	server := Server{
+		core:          gin.New(),
+		configuration: conf,
+	}
+
+	return &server
 }
 
 // Run start listen.
 func (server Server) Run() error {
 	addr := server.configuration.Addr
 	return server.core.Run(addr)
-}
-
-// New return new server instance.
-func New() *Server {
-	return &Server{}
 }
