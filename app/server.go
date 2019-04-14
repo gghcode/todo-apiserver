53ddcf707e15d8a4c3048f78609f4a2fd4a4b1a2
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-04-15 08:49:06 +0900

    Implement server.Run method

--- app/server.go
+++ app/server.go
@@ -2,17 +2,19 @@ package app
 
 import (
 	"github.com/gin-gonic/gin"
+	"gitlab.com/gyuhwan/apas-todo-apiserver/config"
 )
 
 // Server is api-server instance. it contains gin.Engine, middlewares, configuration.
 type Server struct {
-	core *gin.Engine
+	core          *gin.Engine
+	configuration *config.Configuration
 }
 
 // Run start listen.
 func (server Server) Run() error {
-
-	return nil
+	addr := server.configuration.Addr
+	return server.core.Run(addr)
 }
 
 // New return new server instance.
--- config/configuration.go
+++ config/configuration.go
@@ -2,4 +2,5 @@ package config
 
 // Configuration is config type.
 type Configuration struct {
+	Addr string
 }
