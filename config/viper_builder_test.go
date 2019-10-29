1f5c8f6c41e3e7ce7e815c84b45743240f5acb5e
Gyuhwan Kim <gyuhwan.a.kim@gmail.com>
2019-10-29 11:05:56 +0000

    Add unit tests about AddConfigFile method

--- config/viper_builder_test.go
+++ config/viper_builder_test.go
@@ -5,21 +5,45 @@ import (
 	"testing"
 
 	"github.com/gghcode/apas-todo-apiserver/config"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestBuildExpectedDefaultConfig(t *testing.T) {
+type ViperBuilderUnit struct {
+	suite.Suite
+}
+
+func TestConfigViperBuilderUnit(t *testing.T) {
+	suite.Run(t, new(ViperBuilderUnit))
+}
+
+func (suite *ViperBuilderUnit) TestBuildReturnDefaultConfig() {
 	expected := config.DefaultConfig()
 
 	actual, err := config.NewViperBuilder().Build()
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	assert.Equal(t, expected, actual)
+	suite.NoError(err)
+	suite.Equal(expected, actual)
 }
 
-func TestBindEnvs(t *testing.T) {
+func (suite *ViperBuilderUnit) TestAddConfigFileReturnPathErrorWhenOptionalFalse() {
+	_, actualErr := config.NewViperBuilder().
+		AddConfigFile("NOT_EXISTS_CONFIG_FILE.json", false).
+		Build()
+
+	if _, ok := actualErr.(*os.PathError); !ok {
+		suite.T().Fatal(actualErr)
+	}
+}
+
+func (suite *ViperBuilderUnit) TestAddConfigFileReturnNilWhenOptionalTrue() {
+	_, actualErr := config.NewViperBuilder().
+		AddConfigFile("NOT_EXISTS_CONFIG_FILE.json", true).
+		Build()
+
+	suite.NoError(actualErr)
+}
+
+func (suite *ViperBuilderUnit) TestBindEnvs() {
 	expected := config.DefaultConfig()
 	expected.Addr = ":3000"
 
@@ -29,9 +53,6 @@ func TestBindEnvs(t *testing.T) {
 		BindEnvs("TEST2").
 		Build()
 
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	assert.Equal(t, expected, actual)
+	suite.NoError(err)
+	suite.Equal(expected, actual)
 }
